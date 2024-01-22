3685baad3791457cf7700098f20a4f875ad8c931
avcna <avocadobanana33@gmail.com>
2024-01-22 17:18:06 +0700

    add connection ui

--- src/components/ConnectionsComponent.jsx
+++ src/components/ConnectionsComponent.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "../Sass/ConnectionsComponent.scss";
+import { getAllUsers } from "../api/fireStoreAPIs";
 
 const ConnectionsComponent = ({ currentUser }) => {
-  return <div>connection comp</div>;
+  const [allUser, setAllUser] = useState([]);
+  useEffect(() => getAllUsers(setAllUser), []);
+  return (
+    <div className="card">
+      {allUser.map((user) => (
+        <div className="card-item">
+          <div className="wrapper-profile-image">
+            {" "}
+            <img src={user?.imageLink} alt="" className="profile-image" />
+          </div>
+          <p>{user?.name}</p>
+          <p>{user?.headline}</p>
+          <button>Connect</button>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ConnectionsComponent;
